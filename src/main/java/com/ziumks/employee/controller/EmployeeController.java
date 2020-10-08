ff00a5d4926fcbb4a4b8d8dc2c6f0b5a8f254e11
baltop <baltop@gmail.com>
2020-10-08 09:54:38 +0900

    exception

--- src/main/java/com/ziumks/employee/controller/EmployeeController.java
+++ src/main/java/com/ziumks/employee/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,10 +20,19 @@ import com.ziumks.employee.repository.EmployeeRepository;
 @RequestMapping("/employee/*")
 public class EmployeeController {
 
+	
+	private final String profile;
+	
+	
 	private static final Logger LOGGER = LoggerFactory.getLogger(EmployeeController.class);
 	
 	@Autowired
 	EmployeeRepository repository;
+	
+	
+	EmployeeController(@Value("${spring.profiles.active}") String profile) {
+        this.profile = profile;
+    }
 	
 	@PostMapping("/")
 	public Employee add(@RequestBody Employee employee) {
@@ -54,4 +64,33 @@ public class EmployeeController {
 		return repository.findByOrganization(organizationId);
 	}
 	
+    @SuppressWarnings("unused")
+	@GetMapping("/exception")
+    public ResponseJson exception() throws Exception {
+
+        if("service03".equals(profile)) {
+            int i = 1/0;
+        }
+
+        if("service02".equals(profile)) {
+            Thread.sleep(10000);
+        }
+
+        return new ResponseJson("Success from " + profile);
+    }
+
+    private class ResponseJson {
+
+        private final String msg;
+
+
+        private ResponseJson(String msg) {
+            this.msg = msg;
+        }
+
+        @SuppressWarnings("unused")
+		public String getMsg() {
+            return msg;
+        }
+    }
 }
